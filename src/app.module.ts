185cf8285fcfb97731ba7f1274ebf86fd2f6f148
Pablo Bestani <130400091+PabloBestani@users.noreply.github.com>
2024-01-15 15:33:18 -0300

    feat(typeorm): created db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { DogsModule } from './dogs/dogs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DogsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT ? +process.env.POSTGRES_PORT : 5432,
+      autoLoadEntities: true,
+      synchronize: true, //SETEAR EN FALSE en produ
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    }),
+    DogsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/dogs/entities/dog.entity.ts
+++ src/dogs/entities/dog.entity.ts
@@ -1,1 +1,10 @@
-export class Dog {}
+import { Entity } from 'typeorm';
+
+@Entity()
+export class Dog {
+  id: number;
+
+  name: string;
+
+  age: number;
+}
